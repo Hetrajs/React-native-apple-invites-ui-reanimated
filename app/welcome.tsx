ecdba18183f3f2418c2b20b689c7de7905928c9a
Kuldeepsinh Solanki <kuldeepsinhsolanki@KuldeepsinhsMBP.lan>
2025-03-21 21:48:15 +0530

    Welcome screen without Statemanagement

---
+++ app/welcome.tsx
@@ -0,0 +1,86 @@
+import { Image, Pressable, ScrollView, StatusBar, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { BlurView } from "expo-blur"
+
+const events = [
+    {
+        id: 1,
+        image: require("../assets/events/1.png")
+    },
+    {
+        id: 2,
+        image: require("../assets/events/2.png")
+    },
+    {
+        id: 3,
+        image: require("../assets/events/3.png")
+    },
+    {
+        id: 4,
+        image: require("../assets/events/4.png")
+    },
+    {
+        id: 5,
+        image: require("../assets/events/5.png")
+    },
+    {
+        id: 6,
+        image: require("../assets/events/6.png")
+    },
+    {
+        id: 7,
+        image: require("../assets/events/7.png")
+    },
+    {
+        id: 8,
+        image: require("../assets/events/8.png")
+    }
+]
+
+export default function welcomeScreen() {
+    return (
+        <>
+
+            <StatusBar barStyle="light-content" />
+            <View className="flex-1 items-center bg-yellow-950">
+                <Image
+                    source={events[4].image}
+                    className="absolute bottom-0 left-0 right-0 top-0"
+                />
+                <View className="absolute bottom-0 left-0 right-0 top-0 bg-black/50" />
+
+                <BlurView intensity={100}>
+                    <SafeAreaView>
+                    <View className="h-3/5 w-full"> 
+                        <ScrollView
+                            horizontal
+                            >
+                            {
+                                events.map((event) => (
+                                    <View key={event.id} className="h-full w-96 p-5">
+                                        <Image
+                                            source={event.image}
+                                            className="h-full w-full rounded-3xl"
+                                            resizeMode="cover"
+                                            />
+                                    </View>
+                                ))
+                            }
+                        </ScrollView>
+                    </View>
+                    <View className="flex-1 justify-center gap-4 p-4">
+                        <Text className="text-center text-2xl font-bold text-white/60">Welcome to</Text>
+                        <Text className="text-center text-5xl font-bold text-white">RHTR Invites</Text>
+                        <Text className="text-center text-lg text-white/60 ">Create beautiful invitations for your events. Anyone can receive invitations.</Text>
+
+                        <Pressable className="items-center self-center rounded-full bg-white px-10 py-4">
+                            <Text className="text-lg">Create an Event</Text>
+                        </Pressable>
+                    </View>
+                            </SafeAreaView>
+                </BlurView>
+
+            </View>
+        </>
+    )
+}
