3b4e42751f90756a2204f9e0dd7455442643941e
Hetrajs <hetrajsinhraj@gmail.com>
2025-03-27 17:59:03 +0530

    Welocme Screen with Statemanagement

--- app/welcome.tsx
+++ app/welcome.tsx
@@ -1,6 +1,8 @@
 import { Image, Pressable, ScrollView, StatusBar, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { BlurView } from "expo-blur"
+import { useState } from "react";
+import Animated, { FadeIn, FadeOut } from "react-native-reanimated";
 
 const events = [
     {
@@ -38,26 +40,41 @@ const events = [
 ]
 
 export default function welcomeScreen() {
+
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    const onButtonPress = () => {
+        setActiveIndex(activeIndex >= events.length - 1 ? 0 : activeIndex + 1);
+    }
+
     return (
         <>
 
             <StatusBar barStyle="light-content" />
             <View className="flex-1 items-center bg-yellow-950">
-                <Image
-                    source={events[4].image}
-                    className="absolute bottom-0 left-0 right-0 top-0"
+                <Animated.Image
+                    entering={FadeIn.duration(1000)}
+                    exiting={FadeOut.duration(1000)}
+                    key={events[activeIndex].image}
+                    source={events[activeIndex].image}
+                    className="absolute left-0 top-0 h-full w-full"
+                    resizeMode="cover"
                 />
-                <View className="absolute bottom-0 left-0 right-0 top-0 bg-black/50" />
+                <View className="absolute left-0 top-0 h-full w-full bg-black/70" />
 
-                <BlurView intensity={100}>
-                    <SafeAreaView>
+                <BlurView 
+                    intensity={100} 
+                    className="absolute inset-0 flex-1 w-full h-full"
+                >
+                    <SafeAreaView className="flex-1">
                     <View className="h-3/5 w-full"> 
                         <ScrollView
                             horizontal
+                            showsHorizontalScrollIndicator={false}
                             >
                             {
                                 events.map((event) => (
-                                    <View key={event.id} className="h-full w-96 p-5">
+                                    <View key={event.id} className="h-full w-96 p-5 shadow-md">
                                         <Image
                                             source={event.image}
                                             className="h-full w-full rounded-3xl"
@@ -73,11 +90,11 @@ export default function welcomeScreen() {
                         <Text className="text-center text-5xl font-bold text-white">RHTR Invites</Text>
                         <Text className="text-center text-lg text-white/60 ">Create beautiful invitations for your events. Anyone can receive invitations.</Text>
 
-                        <Pressable className="items-center self-center rounded-full bg-white px-10 py-4">
+                        <Pressable onPress={onButtonPress} className="items-center self-center rounded-full bg-white px-10 py-4">
                             <Text className="text-lg">Create an Event</Text>
                         </Pressable>
                     </View>
-                            </SafeAreaView>
+                    </SafeAreaView>
                 </BlurView>
 
             </View>
